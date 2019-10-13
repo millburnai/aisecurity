463c5c1d3f45409903ee994634de74051a9c850d
ryan-ml-rig <22parkr@millburn.org>
2019-10-13 16:24:40 -0400

    adding __inits__

---
+++ aisecurity/__init__.py
@@ -0,0 +1,8 @@
+from . import extras
+from . import logs
+from . import security
+from . import facenet
+
+# importable from root
+from .facenet import FaceNet
+from .extras.paths import HOME
---
+++ aisecurity/extras/__init__.py
@@ -0,0 +1,2 @@
+from . import demo
+from . import paths
