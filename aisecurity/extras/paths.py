1ad122cfe589095b2bd3b66eca536d0d567a97fb
Ryan Park <22parkr@millburn.org>
2019-10-30 16:44:52 -0400

    removing dev support from paths

--- aisecurity/extras/paths.py
+++ aisecurity/extras/paths.py
@@ -15,27 +15,13 @@ from termcolor import cprint
 HOME, CONFIG_HOME = os.getenv("HOME"), os.getenv("HOME")
 os.chdir(HOME)
 
-if os.path.exists(HOME + "/PycharmProjects/aisecurity"):  # for development
-    CONFIG = json.load(open(HOME + "/PycharmProjects/aisecurity/.aisecurity/aisecurity.json"))
-    HOME += "/PycharmProjects/aisecurity/aisecurity"
-    CONFIG_HOME += "/PycharmProjects/aisecurity/.aisecurity"
-    subprocess.call([os.getenv("HOME") + "/PycharmProjects/aisecurity/.aisecurity/update_config.sh"])
+import aisecurity
 
-elif os.path.exists(HOME + "/Desktop/aisecurity"):
-    CONFIG = json.load(open(HOME + "/Desktop/aisecurity/.aisecurity/aisecurity.json"))
-    HOME += "/Desktop/aisecurity/aisecurity"
-    CONFIG_HOME += "/PycharmProjects/aisecurity/.aisecurity"
-    subprocess.call([os.getenv("HOME") + "/PycharmProjects/aisecurity/.aisecurity/update_config.sh"])
-    cprint("LIAM GET PYCHARM", color="white", on_color="on_red", attrs=["bold"])
-
-else:
-    import aisecurity
-
-    CONFIG_HOME += "/.aisecurity"
-    HOME = os.path.abspath(aisecurity.__file__).replace("/__init__.py", "")
-    if not os.path.exists(CONFIG_HOME + "/aisecurity.json"):
-        subprocess.call(["make_config.sh"])
-    CONFIG = json.load(open(CONFIG_HOME + "/aisecurity.json"))
+CONFIG_HOME += "/.aisecurity"
+HOME = os.path.abspath(aisecurity.__file__).replace("/__init__.py", "")
+if not os.path.exists(CONFIG_HOME + "/aisecurity.json"):
+    subprocess.call(["make_config.sh"])
+CONFIG = json.load(open(CONFIG_HOME + "/aisecurity.json"))
 
 DATABASE = CONFIG["database_location"]
 
