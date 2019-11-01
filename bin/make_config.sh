1bd94d2d39095232eb8cb57d33348f127af3cedd
ryan <22parkr@millburn.org>
2019-11-01 00:53:09 -0400

    updating scripts for move to dropbox

--- bin/make_config.sh
+++ bin/make_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # ".aisecurity.make_config"
-# Program to make config file (~/.aisecurity.json)
+# Program to make config file, download models, and download encrypted database
 
 if [ ! -d "$HOME/.aisecurity" ] ; then
   mkdir "$HOME/.aisecurity"
@@ -34,10 +34,10 @@ if [ ! -d "$config_path/models" ] ; then
   cd "$config_path" || echo "Error: unable to access $config_path"
   mkdir models
   cd models || echo "Error: unable to access $config_path/models"
-  wget -O "ms_celeb_1m.h5" "https://drive.google.com/uc?export=download&id=1e2S1lPVscLYUpsEPP4BP-x5btHOfNtfJ" \
+  wget -O "ms_celeb_1m.h5" "https://www.dropbox.com/s/i4r3jbnzuzcc9fh/ms_celeb_1m.h5?dl=1" \
   || echo "Error: MS-Celeb-1M model could not be downloaded"
-  wget -O "vgg_face_2.h5" "https://drive.google.com/uc?export=download&id=1_HrwcVG41qCi2x7MI8CLMtu_mzi4WxE_" \
-  || echo "Error: VGGFace2 could not be downloaded"
+  wget -O "vgg_face_2.h5" "https://www.dropbox.com/s/4xo8uuhu9ug8ir3/vgg_face_2.h5?dl=1" \
+  || echo "Error: VGGFace2 model could not be downloaded"
 fi
 
 if [ ! -d "$HOME/.aisecurity/keys" ] ; then
