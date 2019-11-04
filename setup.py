790fa085d81a04cd68c9d1bf1bf2d80fc296f503
Ryan Park <22parkr@millburn.org>
2019-11-04 12:50:57 -0500

    temporarily rm tf from req for jetson nano install

--- setup.py
+++ setup.py
@@ -10,8 +10,7 @@ setup(
     author_email="22parkr@millburn.org, 22pilarskil@millburn.org",
     license=None,
     python_requires=">=3.5.0",
-    install_requires=["numpy", "matplotlib", "termcolor", "scikit-learn", "imageio", "mtcnn", "pycryptodome",
-                      "tensorflow<=1.15.0"],
+    install_requires=["numpy", "matplotlib", "termcolor", "scikit-learn", "imageio", "mtcnn", "pycryptodome"],
     packages=find_packages(),
     scripts=["bin/make_config.sh", "bin/make_keys.sh"],
     zip_safe=False
