9d3dc90cf198308ab8e4d65b90361810d88e78c3
Ryan Park <22parkr@millburn.org>
2019-11-05 15:08:19 -0500

    trying to patch picam keras support

--- aisecurity/facenet.py
+++ aisecurity/facenet.py
@@ -233,13 +233,24 @@ class FaceNet(object):
     @staticmethod
     def get_video_cap(picamera):
         def _gstreamer_pipeline(capture_width=1280, capture_height=720, display_width=1280, display_height=720,
-                                framerate=60, flip_method=0):
+                                framerate=20, flip_method=0):
             return (
-                    "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, format=(string)NV12,"
-                    " framerate=(fraction)%d/1 ! nvvidconv flip-method=%d ! video/x-raw, width=(int)%d, height=(int)%d,"
-                    " format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink"
-                    % (capture_width, capture_height, framerate, flip_method, display_width, display_height)
-            )
+                "nvarguscamerasrc ! "
+                "video/x-raw(memory:NVMM), "
+                "width=(int)%d, height=(int)%d, "
+                "format=(string)NV12, framerate=(fraction)%d/1 ! "
+                "nvvidconv flip-method=%d ! "
+                "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+                "videoconvert ! "
+                "video/x-raw, format=(string)BGR ! appsink"
+                % (
+                    capture_width,
+                    capture_height,
+                    framerate,
+                    flip_method,
+                    display_width,
+                    display_height,
+                ))
 
         if picamera:
             return cv2.VideoCapture(_gstreamer_pipeline(), cv2.CAP_GSTREAMER)
