c6d226bc1862425123d6b38d44beda8b24babad8
ryan <22parkr@millburn.org>
2019-11-07 14:27:41 -0500

    adding freeze_graph to utils

--- aisecurity/extras/utils.py
+++ aisecurity/extras/utils.py
@@ -11,6 +11,8 @@ Data utils and assorted functions.
 import functools
 import time
 
+import tensorflow as tf
+from tensorflow.python.framework import graph_io
 import tensorrt as trt
 
 
@@ -85,8 +87,52 @@ class CudaEngineManager(object):
         with open(target_file, "wb") as file:
             file.write(self.engine)
 
+# FREEZE GRAPH
+@timer("Freezing time")
+def freeze_graph(path_to_model, save_dir=None, save_name="frozen_graph.pb"):
+
+    tf.keras.backend.clear_session()
+
+    def _freeze_graph(graph, session, output):
+        with graph.as_default():
+            variable = tf.graph_util.remove_training_nodes(graph.as_graph_def())
+            frozen = tf.graph_util.convert_variables_to_constants(session, variable, output)
+            return frozen
+
+    tf.keras.backend.set_learning_phase(0)
+
+    if path_to_model.endswith(".h5"):
+        model = tf.keras.models.load_model(path_to_model)
+    elif path_to_model.endswith(".pb"):
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        with tf.Session(config=config) as sess:
+            with tf.gfile.FastGFile(path_to_model, "rb") as graph_file:
+                graph_def = tf.GraphDef()
+                graph_def.ParseFromString(graph_file.read())
+                sess.graph.as_default()
+                tf.import_graph_def(graph_def, name="")
+                model = sess.graph
+    else:
+        raise ValueError("{} must be a .h5 or a .pb file")
+
+    session = tf.keras.backend.get_session()
+
+    input_names = [layer.op.name for layer in model.inputs]
+    output_names = [layer.op.name for layer in model.outputs]
+
+    frozen_graph = _freeze_graph(session.graph, session, output_names)
+    if save_dir:
+        graph_io.write_graph(frozen_graph, save_dir, save_name, as_text=False)
+
+    return frozen_graph, (input_names, output_names)
+
+
 
 if __name__ == "__main__":
+    freeze_graph("/home/ryan/scratchpad/aisecurity/models/sandberg.pb",
+                 save_dir="/home/ryan/scratchpad/aisecurity/models",
+                 save_name="frozen_sandberg.pb")
     print("Nothing for now!")
     c = CudaEngineManager()
     c.write_cuda_engine("/home/ryan/scratchpad/aisecurity/models/test.engine",
