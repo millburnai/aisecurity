0fd452e5c6c5f4523c57a9f4c9db33399d61c74c
ryan <22parkr@millburn.org>
2019-11-08 19:57:31 -0500

    adding caffe to engine support to utils

--- aisecurity/extras/utils.py
+++ aisecurity/extras/utils.py
@@ -3,12 +3,13 @@
 
 "aisecurity.extras.utils"
 
-Data utils and assorted functions.
+Data and CUDA utils.
 
 """
 
 
 import functools
+import subprocess
 import time
 
 import tensorflow as tf
@@ -43,7 +44,7 @@ class CudaEngineManager(object):
     }
 
 
-    # INITS
+    # INIT
     def __init__(self, **kwargs):
         self.CONSTANTS = {**self.CONSTANTS, **kwargs}
 
@@ -56,67 +57,105 @@ class CudaEngineManager(object):
 
         self.network = self.builder.create_network()
 
-    # CUDA ENGINE MANAGEMENT
+
+    # ENGINE TASK HELPERS
+    @staticmethod
+    @timer("Uff model parsing time")
+    def _parse_uff(uff_file, network, input_name, input_shape, output_name):
+        parser = trt.UffParser()
+
+        parser.register_input(input_name, input_shape)
+        parser.register_output(output_name)
+
+        parser.parse(uff_file, network, CudaEngineManager.CONSTANTS["dtype"])
+
+        return parser
+
+    @staticmethod
+    @timer("Caffe model parsing time")
+    def _parse_caffe(caffe_model_file, caffe_deploy_file, network, output_name="prob1"):
+        parser = trt.CaffeParser()
+
+        model_tensors = parser.parse(deploy=caffe_deploy_file, model=caffe_model_file, network=network,
+                                     dtype=CudaEngineManager.CONSTANTS["dtype"])
+
+        network.mark_output(model_tensors.find(output_name))
+
+        return parser
+
+    @staticmethod
+    @timer("Engine building time")
+    def _build_engine(builder, network):
+        return builder.build_cuda_engine(network)
+
+    @staticmethod
+    @timer("Engine serializing time")
+    def _serialize_engine(engine):
+        return engine.serialize()
+
+
+    # CUDA ENGINE WRITE AND READ
     def read_cuda_engine(self, engine_file):
         with open(engine_file, "rb") as file, trt.Runtime(self.CONSTANTS["trt_logger"]) as runtime:
             self.engine = runtime.deserialize_cuda_engine(file.read())
 
-    def write_cuda_engine(self, uff_file, target_file, input_name, input_shape, output_name):
-
-        @timer("Model parsing time")
-        def parse(parser, uff_file, network, input_name, input_shape, output_name):
-            parser.register_input(input_name, input_shape)
-            parser.register_output(output_name)
-
-            parser.parse(uff_file, network, self.CONSTANTS["dtype"])
-
-            return parser
-
-        @timer("Engine building time")
-        def build_engine(builder, network):
-            return builder.build_cuda_engine(network)
-
-        @timer("Engine serializing time")
-        def serialize_engine(engine):
-            return engine.serialize()
-
-        self.parser = parse(trt.UffParser(), uff_file, self.network, input_name, input_shape, output_name)
-
-        self.engine = serialize_engine(build_engine(self.builder, self.network))
+    def uff_write_cuda_engine(self, uff_file, target_file, input_name, input_shape, output_name):
+        self.parser = self._parse_uff(uff_file, self.network, input_name, input_shape, output_name)
+        self.engine = self._serialize_engine(self._build_engine(self.builder, self.network))
 
         with open(target_file, "wb") as file:
             file.write(self.engine)
 
-# FREEZE KERAS MODEL AS FROZEN GRAPH
-@timer("Freezing time")
-def keras_to_frozen(path_to_model, save_dir=None, save_name="frozen_graph.pb"):
+    def caffe_write_cuda_engine(self, caffe_model_file, caffe_deploy_file, target_file):
+        self.parser = self._parse_caffe(caffe_model_file, caffe_deploy_file, self.network)
+        self.engine = self._serialize_engine(self._build_engine(self.builder, self.network))
 
-    tf.keras.backend.clear_session()
+        with open(target_file, "wb") as file:
+            file.write(self.engine)
 
-    def _freeze_graph(graph, session, output):
-        with graph.as_default():
-            variable = tf.graph_util.remove_training_nodes(graph.as_graph_def())
-            frozen = tf.graph_util.convert_variables_to_constants(session, variable, output)
-            return frozen
 
-    tf.keras.backend.set_learning_phase(0)
+# MODEL CONVERSIONS
+class GraphConverter(object):
 
-    if path_to_model.endswith(".h5"):
-        model = tf.keras.models.load_model(path_to_model)
-    else:
-        raise ValueError("{} must be a .h5 or a .pb file")
+    # .h5 -> .pb
+    @staticmethod
+    @timer("Freezing time")
+    def keras_to_frozen(path_to_model, save_dir=None, save_name="frozen_graph.pb"):
 
-    session = tf.keras.backend.get_session()
+        tf.keras.backend.clear_session()
 
-    input_names = [layer.op.name for layer in model.inputs]
-    output_names = [layer.op.name for layer in model.outputs]
+        def _freeze_graph(graph, session, output):
+            with graph.as_default():
+                variable = tf.graph_util.remove_training_nodes(graph.as_graph_def())
+                frozen = tf.graph_util.convert_variables_to_constants(session, variable, output)
+                return frozen
 
-    frozen_graph = _freeze_graph(session.graph, session, output_names)
-    if save_dir:
-        graph_io.write_graph(frozen_graph, save_dir, save_name, as_text=False)
+        tf.keras.backend.set_learning_phase(0)
 
-    return frozen_graph, (input_names, output_names)
+        if path_to_model.endswith(".h5"):
+            model = tf.keras.models.load_model(path_to_model)
+        else:
+            raise ValueError("{} must be a .h5 file")
 
+        session = tf.keras.backend.get_session()
+
+        input_names = [layer.op.name for layer in model.inputs]
+        output_names = [layer.op.name for layer in model.outputs]
+
+        frozen_graph = _freeze_graph(session.graph, session, output_names)
+        if save_dir:
+            graph_io.write_graph(frozen_graph, save_dir, save_name, as_text=False)
+
+        return frozen_graph, (input_names, output_names)
+
+    # .pb -> .uff
+    @staticmethod
+    @timer("Conversion to .uff time")
+    def frozen_to_uff(path_to_model):
+        bash_cmd = "convert-to-uff \"{}\"".format(path_to_model)
+        process = subprocess.Popen(bash_cmd.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        return output, error
 
 
 if __name__ == "__main__":
