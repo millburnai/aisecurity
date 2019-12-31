a316d5e6912f08d29bb73d4e7ef136083c136486
ryan <22parkr@millburn.org>
2019-12-31 16:12:33 -0500

    updating links and refactoring make_config.sh

--- bin/make_config.sh
+++ bin/make_config.sh
@@ -18,15 +18,14 @@ config_path=$(pwd )
 echo "Adding aisecurity.json to .aisecurity"
 touch "$HOME/.aisecurity/aisecurity.json"
 
-printf '{\n    "key_directory": "%s/keys/",\n    "key_location": "%s/keys/keys_file.json",\n    "database_location": "%s/database/encrypted.json",\n    "mysql_user": "root",\n    "mysql_password": "root"\n}\n' \
+printf '{\n    "key_directory": "%s/keys/",\n    "key_location": "%s/keys/keys_file.json",\n    "database_location": "%s/database/test.json",\n    "mysql_user": "root",\n    "mysql_password": "root"\n}\n' \
 "$config_path" "$config_path" "$config_path" > "$config_path/aisecurity.json"
 
 if [ ! -d "$config_path/database" ] ; then
   echo "Making database and unknown directories"
   mkdir database
   cd "$config_path/database" || echo "Error: unable to access $config_path/database"
-  touch encrypted.json
-  echo "Fill in $config_path/database/encrypted.json to use aisecurity"
+  wget -O "test.json" https://www.dropbox.com/s/uhii2vj373y7odj/test.json?dl=0 || echo "Error: unable to download test.json"
 fi
 
 if [ ! -d "$config_path/models" ] ; then
@@ -38,7 +37,7 @@ if [ ! -d "$config_path/models" ] ; then
   || echo "Error: MS-Celeb-1M model could not be downloaded"
   wget -O "vgg_face_2.h5" "https://www.dropbox.com/s/4xo8uuhu9ug8ir3/vgg_face_2.h5?dl=1" \
   || echo "Error: VGGFace2 model could not be downloaded"
-  wget -O "haarcascade_frontalface_default.xml" "https://www.dropbox.com/s/mk75ue9k65u0sx9/haarcascade_frontalface_default.xml?dl=0" \
+  wget -O "haarcascade_frontalface_default.xml" "https://www.dropbox.com/s/zhb4cn9idl6rrvm/haarcascade_frontalface_default.xml?dl=1" \
   || echo "Error: haarcascade model could not be downloaded"
 fi
 
@@ -53,8 +52,10 @@ if [ ! -d "$HOME/.aisecurity/bin" ] ; then
   cd "$config_path" || echo "Error: unable to access $config_path"
   mkdir bin
   cd "$config_path/bin" || echo "Error: unable to access $config_path/bin"
-  wget -O "drop.sql" "https://github.com/orangese/aisecurity/raw/tensorrt/bin/drop.sql" || \
+  wget -O "drop.sql" "https://github.com/orangese/aisecurity/raw/v0.9a/bin/drop.sql" || \
   echo "Error: drop.sql could not be downloaded"
+  wget -O "dump_embeds.sh" "https://raw.githubusercontent.com/orangese/aisecurity/v0.9a/bin/dump_embeds.sh" || \
+  echo "Error: dump_embeds.sh could not be downloaded"
 fi
 
 if [ ! -d "$HOME/.aisecurity/logging" ] ; then
@@ -65,12 +66,4 @@ if [ ! -d "$HOME/.aisecurity/logging" ] ; then
   touch firebase.json
   echo "Fill in '$config_path/logging/firebase.json' and a key file in the same directory to use firebase logging"
   mkdir unknown
-fi
-
-if [ ! -f "$HOME/.aisecurity/bin/dump_embeds" ] ; then
-  echo "Importing dump_embeds script"
-  cd "$config_path/bin" || echo "Error: unable to access $config_path/bin"
-  wget -O "dump_embeds.sh" "https://raw.githubusercontent.com/orangese/aisecurity/v0.9a/bin/dump_embeds.sh" || \
-  echo "Error: dumb_embeds.sh could not be downloaded"
-  sudo chmod +x "dump_embeds.sh"
 fi
