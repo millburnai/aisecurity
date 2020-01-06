098adb631d4d76b0b9b5d530b86c2635e06776dd
ryan <22parkr@millburn.org>
2020-01-05 19:47:34 -0500

    adding construct_dict funcs, support for subtract_mean

--- aisecurity/utils/distance.py
+++ aisecurity/utils/distance.py
@@ -11,12 +11,44 @@ import warnings
 import numpy as np
 
 
-# HELPERS
+# LAMBDA DICTS
+_FORMAT = {
+    "apply_to": lambda x: callable(x),
+    "use_stat": lambda x: isinstance(x, bool),
+    "func": lambda x: callable(x)
+}
+
 _CHECKS = {
     "is_vector": lambda x: x.ndim <= 2 and (1 in x.shape or x.ndim == 1),
     "is_float_like": lambda x: isinstance(x, float) or isinstance(x, np.float),
     "is_shape_equal": lambda a, b: np.array(a).shape == np.array(b).shape
 }
+
+
+# FUNCTIONAL DICT CONSTRUCTORS
+def construct_dist(func):
+    def test(test_case):
+        res = func(*test_case)
+        assert isinstance(res, tuple) and len(res) == 2, "func must take two arrays as input and return two arrays"
+
+    test_case = np.random.random((2, ))
+    test(test_case)
+
+    test_case = np.random.random((2, 1))
+    test(test_case)
+
+    return func
+
+
+def construct_norm(**kwargs):
+    checked = kwargs.copy()
+
+    for value, test in _FORMAT.items():
+        assert value in kwargs and test(kwargs[value]), "{} is missing or failed test".format(value)
+        kwargs.pop(value)
+    assert all(callable(extra) for extra in kwargs)
+
+    return checked
 
 
 # DISTMETRIC
@@ -25,67 +57,60 @@ class DistMetric:
     DISTS = {
         # dists are the transformations to be applied before Euclidean distance is calculated
         # ex: for "euclidean", no transformation is applied --> lambda a, b: (a, b)
-        "euclidean": lambda a, b: (a, b),
-        "cosine": lambda a, b: (a, b)  # not done yet
+        "euclidean": construct_dist(
+            lambda a, b: (a, b)
+        ),
+        "cosine": construct_dist(
+            lambda a, b: (
+                a - np.mean(a) / np.maximum(np.std(a), 1e-6),
+                b - np.mean(b) / np.maximum(np.std(b), 1e-6)
+            )  # not so sure this is right... has to be a transformation s.t. x^T x = 1 for all x in a, b
+        )
     }
 
     NORMALIZATIONS = {
-        # normalizations are applied before mode lambdas
-        # ex: for "cosine+l2_normalize", L2 normalization is applied, then cosine normalization, then L2 distance
-        "subtract_mean": {
-            "apply_to": _CHECKS["is_vector"],
-            "use_stat": True,
-            "func": lambda x, stats: x - stats["mean"]
-        },
-        "l2_normalize": {
-            "apply_to": _CHECKS["is_vector"],
-            "use_stat": False,
-            "func": lambda x: x / np.sqrt(np.maximum(np.sum(np.square(x), axis=-1, keepdims=True), 1e-6))
-        },
-        "sigmoid": {
-            "apply_to": _CHECKS["is_float_like"],
-            "use_stat": False,
-            "func": lambda x: 1. / (1. + np.exp(-x))
-        }
+        # normalizations are applied before dist lambdas
+        # ex: for "cosine+l2_normalize", L2 normalization is applied, then cosine normalization, then Euclidean distance
+        "subtract_mean": construct_norm(
+            apply_to= _CHECKS["is_vector"],
+            use_stat=True,
+            func=lambda x, stats: x - stats["mean"]
+        ),
+
+        "l2_normalize": construct_norm(
+            apply_to=_CHECKS["is_vector"],
+            use_stat=False,
+            func=lambda x: x / np.sqrt(np.maximum(np.sum(np.square(x), axis=-1, keepdims=True), 1e-6))
+        ),
+        "sigmoid": construct_norm(
+            apply_to=_CHECKS["is_float_like"],
+            use_stat=False,
+            func=lambda x: 1. / (1. + np.exp(-x))
+        )
     }
 
 
     # INITS
-    def __init__(self, mode, normalizations=None, data=None):
-        # if mode and norms are from defaults
-        if isinstance(mode, str):
-            if "+" in mode:
-                # ex: DistMetric("euclidean+subtract_mean+l2_normalize")
-                self.mode, *self.normalizations = mode.split("+")
-            else:
-                # ex: DistMetric("euclidean", ["subtract_mean", "l2_normalize"])
-                self.mode = mode
-                self.normalizations = normalizations if normalizations else []
+    def __init__(self, dist, normalizations=None, data=None, **kwargs):
+        if "+" in dist:
+            # ex: DistMetric("euclidean+subtract_mean+l2_normalize")
+            self.dist, *self.normalizations = dist.split("+")
+        else:
+            # ex: DistMetric("euclidean", ["subtract_mean", "l2_normalize"])
+            self.dist = dist
+            self.normalizations = normalizations if normalizations else []
 
-            assert self.mode in self.DISTS, "supported modes are {}".format(list(self.DISTS.keys()))
-            assert self.normalizations is None or all(norm in self.NORMALIZATIONS for norm in self.normalizations), \
-                "supported normalizations are {}".format(list(self.NORMALIZATIONS.keys()))
-
-        # if custom mode and norms are provided
-        elif callable(mode):
-            self.mode = mode
-            self.normalizations = normalizations
-
-            test_case = np.random.random((2, ))
-            try:
-                assert isinstance(self.__call__(*test_case), float)
-            except Exception:
-                raise ValueError("check failed: check that custom mode and normalizations are in the correct format")
-
-            warnings.warn("custom mode and normalizations are not supported in FaceNet")
+        assert self.dist in self.DISTS, "supported dists are {}".format(list(self.DISTS.keys()))
+        assert self.normalizations is None or all(norm in self.NORMALIZATIONS for norm in self.normalizations), \
+            "supported normalizations are {}".format(list(self.NORMALIZATIONS.keys()))
 
         # data setting
         if any(self.NORMALIZATIONS[norm_id]["use_stat"] for norm_id in self.normalizations):
             assert data is not None, "data must be provided for normalizations that use data statistics"
 
             self.stats = {
-                "mean": np.mean(data),
-                "std": np.std(data)
+                "mean": np.mean(data, **kwargs),
+                "std": np.std(data, **kwargs)
             }
 
         self.data = data
@@ -96,10 +121,7 @@ class DistMetric:
         normalized = arg
 
         for norm_id in self.normalizations:
-            try:
-                functional_dict = self.NORMALIZATIONS[norm_id]
-            except (KeyError, TypeError):  # will arise if custom funcs provided
-                functional_dict = norm_id
+            functional_dict = self.NORMALIZATIONS[norm_id]
 
             actions = functional_dict.copy()
             apply_to = actions.pop("apply_to")
@@ -130,7 +152,7 @@ class DistMetric:
     # MAGIC FUNCTIONS
     def __call__(self, *args, mode="apply_norm"):
         if mode == "apply_norm":
-            assert len(args) == 1, "'apply_norm' mode requires one arg only, got {} args".format(len(args))
+            assert len(args) == 1, "'apply_norm' dist requires one arg only, got {} args".format(len(args))
 
             arg = args[0]
             arg = arg.reshape(np.array(arg).shape if np.array(arg).shape != () else (-1, 1))
@@ -139,7 +161,7 @@ class DistMetric:
 
         elif "calc" in mode:
             # always use the Euclidean norm because the K-NN algorithm will always use it (pyfuncs are too slow)
-            assert len(args) == 2, "'calc' mode requires two args only, got {} arg(s)".format(len(args))
+            assert len(args) == 2, "'calc' dist requires two args only, got {} arg(s)".format(len(args))
 
             a, b = args
 
@@ -153,9 +175,9 @@ class DistMetric:
 
     def __str__(self):
         result = "Distance ("
-        result += self.mode.__str__()  # __str__ to account for possibility of mode/normalizations being custom funcs
+        result += self.dist
         for norm_id in self.normalizations:
-            result += "+{}".format(norm_id.__str__())
+            result += "+{}".format(norm_id)
         result += ")"
         return result
 
