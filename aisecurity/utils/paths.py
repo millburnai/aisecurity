4e865ace2357ff00e8aba5af34528bf9aaa913bf
ryan <22parkr@millburn.org>
2020-01-08 11:54:07 -0500

    adding missed call to make_keys.sh

--- aisecurity/utils/paths.py
+++ aisecurity/utils/paths.py
@@ -14,8 +14,9 @@ import aisecurity
 
 CONFIG_HOME = os.getenv("HOME") + "/.aisecurity"
 HOME = os.path.abspath(aisecurity.__file__).replace("/__init__.py", "")
-if not os.path.exists(CONFIG_HOME + "/aisecurity.json"):
+if not os.path.exists(CONFIG_HOME):
     subprocess.call(["make_config.sh"])
+    subprocess.call(["make_keys.sh"])
 CONFIG = json.load(open(CONFIG_HOME + "/aisecurity.json"))
 
 DATABASE = CONFIG["database_location"]
@@ -26,3 +27,5 @@ KEY_FILE = CONFIG["key_location"]
 
 NAME_KEYS = json.load(open(KEY_FILE))["names"]
 EMBEDDING_KEYS = json.load(open(KEY_FILE))["embeddings"]
+
+DEFAULT_MODEL = CONFIG["default_model"]
