e1e9151bafc17ce8f81976e456a09b0955eba5ee
ryan <22parkr@millburn.org>
2020-01-17 20:14:20 -0500

    Fix boolean flags in argparse

--- aisecurity/samples/demo.py
+++ aisecurity/samples/demo.py
@@ -38,14 +38,6 @@ if __name__ == "__main__":
 
 
     # TYPE CASTING
-    def to_bool(string):
-        if string.lower() in ("yes", "true", "t", "y", "1"):
-            return True
-        elif string.lower() in ("no", "false", "f", "n", "0"):
-            return False
-        else:
-            raise argparse.ArgumentTypeError("boolean value expected")
-
     def bounded_float(string):
         if 0. <= float(string) <= 1.:
             return float(string)
@@ -72,23 +64,23 @@ if __name__ == "__main__":
     parser.add_argument("--dist_metric", help="distance metric (default: auto)", type=str, default="auto")
     parser.add_argument("--logging", help="logging type, mysql or firebase (default: None)", type=none_or_str,
                         default=None)
-    parser.add_argument("--use_dynamic", help="use dynamic database (default: True)", type=to_bool, default=True)
-    parser.add_argument("--use_graphics", help="display graphics (default: True)", type=to_bool, default=True)
-    parser.add_argument("--use_picam", help="use Picamera (default: False)", type=to_bool, default=False)
-    parser.add_argument("--use_lcd", help="use LCD display (default: False)", type=to_bool, default=False)
-    parser.add_argument("--use_keypad", help="use keypad display (default: False)", type=to_bool, default=False)
+    parser.add_argument("--use_dynamic", help="use this flag to use dynamic database", action="store_true")
+    parser.add_argument("--no_graphics", help="use this flag to turn off graphics", action="store_true")
+    parser.add_argument("--use_picam", help="use this flag to use a Picamera", action="store_true")
+    parser.add_argument("--use_lcd", help="use this flag to use LCD display", action="store_true")
+    parser.add_argument("--use_keypad", help="use this flag to use keypad", action="store_true")
     parser.add_argument("--flip", help="flip method: +1 = +90º rotation (default: 0)", type=to_int, default=0)
     parser.add_argument("--resize", help="resize frame for faster recognition (default: None)", type=bounded_float,
                         default=None)
     parser.add_argument("--device", help="camera device (default: 0)", type=to_int, default=0)
-    parser.add_argument("--allow_gpu_growth", help="GPU growth (default: False)", type=to_bool, default=False)
+    parser.add_argument("--allow_gpu_growth", help="use this flag to use GPU growth", action="store_true")
     args = parser.parse_args()
 
 
     # DEMO
     demo(
         path=args.path_to_model, dist_metric=args.dist_metric, logging=args.logging,
-        use_dynamic=args.use_dynamic, use_picam=args.use_picam, use_graphics=args.use_graphics,
+        use_dynamic=args.use_dynamic, use_picam=args.use_picam, use_graphics=not args.no_graphics,
         use_lcd=args.use_lcd, use_keypad=args.use_keypad, flip=args.flip, resize=args.resize, device=args.device,
         allow_gpu_growth=args.allow_gpu_growth
     )
