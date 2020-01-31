3c9af8ef72b5e5798075a50cfb5d51f1327f1f96
Ryan Park <22parkr@millburn.org>
2020-01-31 08:01:47 -0500

    Fix ANSI escape codes for shell scripts

--- bin/make_config.sh
+++ bin/make_config.sh
@@ -7,69 +7,86 @@ if [ ! -d "$HOME/.aisecurity" ] ; then
   mkdir "$HOME/.aisecurity"
 fi
 
-cd "$HOME/.aisecurity" || echo -e "\e[31mError: unable to access ~/.aisecurity\e[0m"
+cd "$HOME/.aisecurity" || echo -e "\033[0;31mError: unable to access ~/.aisecurity\033[0m"
 config_path=$(pwd )
 
 if [ ! -f "$HOME/.aisecurity/aisecurity.json" ] ; then
-  echo -e "\e[95mAdding aisecurity.json to .aisecurity\e[0m"
+  echo -e "\033[0;95mAdding aisecurity.json to .aisecurity\033[0m"
   touch "$HOME/.aisecurity/aisecurity.json"
   printf '{\n    "default_model": "%s/models/ms_celeb_1m.h5",\n    "key_directory": "%s/keys/",\n    "key_location": "%s/keys/keys_file.json",\n    "database_location": "%s/database/test.json",\n    "database_info": "%s/database/test_info.json",\n    "mysql_user": "root",\n    "mysql_password": "root"\n}\n' \
   "$config_path" "$config_path" "$config_path" "$config_path" "$config_path" > "$config_path/aisecurity.json"
 fi
 
 if [ ! -d "$config_path/database" ] ; then
-  echo -e "\e[95mMaking database and unknown directories\e[0m"
+  echo -e "\033[0;95mMaking database and unknown directories\033[0m"
   mkdir database
-  cd "$config_path/database" || echo -e "\e[31mError: unable to access $config_path/database\e[0m"
-  curl -Lo "test.json" "https://www.dropbox.com/s/umjku76xppc0396/test.json?dl=1" \
-  || echo -e "\e[31mError: unable to download test.json\e[0m"
-  curl -Lo "test_info.json" "https://www.dropbox.com/s/ihfmemt6sqdfj74/test_info.json?dl=1" \
-  || echo -e "\e[31mError: unable to download test_info.json\e[0m"
+  cd "$config_path/database" || echo -e "\033[0;31mError: unable to access $config_path/database\033[0m"
+
+  if [ ! -f "$config_path/database/test.json" ] ; then
+    echo -e "\033[0;95mDownloading mini-database and its config\033[0m"
+    curl -Lo "test.json" "https://www.dropbox.com/s/umjku76xppc0396/test.json?dl=1" \
+    || echo -e "\033[0;31mError: unable to download test.json\033[0m"
+    curl -Lo "test_info.json" "https://www.dropbox.com/s/ihfmemt6sqdfj74/test_info.json?dl=1" \
+    || echo -e "\033[0;31mError: unable to download test_info.json\033[0m"
+  fi
+
 fi
 
 if [ ! -d "$config_path/models" ] ; then
-  echo -e "\e[95mDownloading Facenet models\e[0m"
-  cd "$config_path" || echo -e "\e[31mError: unable to access $config_path\e[0m"
+  echo -e "\033[0;95mDownloading Facenet models\033[0m"
+  cd "$config_path" || echo -e "\033[0;31mError: unable to access $config_path\033[0m"
   mkdir models
-  cd models || echo -e "\e[31mError: unable to access $config_path/models\e[0m"
-  curl -Lo "ms_celeb_1m.h5" "https://www.dropbox.com/s/i4r3jbnzuzcc9fh/ms_celeb_1m.h52?dl=1" \
-  || echo -e "\e[31mError: MS-Celeb-1M model could not be downloaded\e[0m"
-  curl -Lo "haarcascade_frontalface_default.xml" "https://www.dropbox.com/s/zhb4cn9idl6rrvm/haarcascade_frontalface_default.xml?dl=1" \
-  || echo -e "\e[31mError: haarcascade model could not be downloaded\e[0m"
+  cd models || echo -e "\033[0;31mError: unable to access $config_path/models\033[0m"
+
+  if [ ! -f "$config_path/models/ms_celeb_1m.h5" ] ; then
+    curl -Lo "ms_celeb_1m.h5" "https://www.dropbox.com/s/i4r3jbnzuzcc9fh/ms_celeb_1m.h52?dl=1" \
+    || echo -e "\033[0;31mError: MS-Celeb-1M model could not be downloaded\033[0m"
+  fi
+  if [ ! -f "$config_path/models/haarcascade_frontalface_default.xml" ] ; then
+    curl -Lo "haarcascade_frontalface_default.xml" "https://www.dropbox.com/s/zhb4cn9idl6rrvm/haarcascade_frontalface_default.xml?dl=1" \
+    || echo -e "\033[0;31mError: haarcascade model could not be downloaded\033[0m"
+  fi
+
 fi
 
 if [ ! -d "$HOME/.aisecurity/keys" ] ; then
-  echo -e "\e[95mCreating keys directory\e[0m"
-  cd "$config_path" || echo -e "\e[31mError: unable to access $config_path\e[0m"
+  echo -e "\033[0;95mCreating keys directory\033[0m"
+  cd "$config_path" || echo -e "\033[0;31mError: unable to access $config_path\033[0m"
   mkdir keys
 fi
 
 if [ ! -d "$HOME/.aisecurity/bin" ] ; then
-  echo -e "\e[95mCreating bin directory\e[0m"
-  cd "$config_path" || echo -e "\e[31mError: unable to access $config_path\e[0m"
+  echo -e "\033[0;95mCreating bin directory\033[0m"
+  cd "$config_path" || echo -e "\033[0;31mError: unable to access $config_path\033[0m"
   mkdir bin
-  cd "$config_path/bin" || echo -e "\e[31mError: unable to access $config_path/bin\e[0m"
+  cd "$config_path/bin" || echo -e "\033[0;31mError: unable to access $config_path/bin\033[0m"
 fi
 
 if [ ! -d "$HOME/.aisecurity/logging" ] ; then
-  echo -e "\e[95mCreating logging directory\e[0m"
-  cd "$config_path" || echo -e "\e[31mError: unable to access $config_path\e[0m"
+  echo -e "\033[0;95mCreating logging directory\033[0m"
+  cd "$config_path" || echo -e "\033[0;31mError: unable to access $config_path\033[0m"
   mkdir logging
-  cd "$config_path/logging" || echo -e "\e[31mError: unable to access $config_path/logging\e[0m"
+  cd "$config_path/logging" || echo -e "\033[0;31mError: unable to access $config_path/logging\033[0m"
   touch "$config_path/logging/firebase.json"
-  echo -e "\e[31mFill in '$config_path/logging/firebase.json' and a key file in the same directory to use firebase logging\e[0m"
+  echo -e "\033[0;31mFill in '$config_path/logging/firebase.json' and a key file in the same directory to use firebase logging\033[0m"
   mkdir unknown
 fi
 
 if [ ! -d "$HOME/.aisecurity/config" ] ; then
-  echo -e "\e[95mCreating config directory\e[0m"
-  cd "$config_path" || echo -e "\e[31mError: unable to access $config_path\e[0m"
+  echo -e "\033[0;95mCreating config directory\033[0m"
+  cd "$config_path" || echo -e "\033[0;31mError: unable to access $config_path\033[0m"
   mkdir config
-  cd "$config_path/config" || echo -e "\e[31mError: unable to access $config_path/config\e[0m"
-  curl -Lo "models.json" "https://www.dropbox.com/s/9my8ofbzohi0dsm/models.json?dl=1" \
-  || echo -e "\e[31mError: unable to download models.json\e[0m"
-  curl -Lo "cuda_models.json" "https://www.dropbox.com/s/ieke59ny0r7qxo3/cuda_models.json?dl=1" \
-  || echo -e "\e[31mError: unable to download cuda_models.json\e[0m"
+  cd "$config_path/config" || echo -e "\033[0;31mError: unable to access $config_path/config\033[0m"
+
+  if [ ! -f "models.json" ] ; then
+    curl -Lo "models.json" "https://www.dropbox.com/s/9my8ofbzohi0dsm/models.json?dl=1" \
+    || echo -e "\033[0;31mError: unable to download models.json\033[0m"
+  fi
+  if [ ! -f "cuda_models.json" ] ; then
+    curl -Lo "cuda_models.json" "https://www.dropbox.com/s/ieke59ny0r7qxo3/cuda_models.json?dl=1" \
+    || echo -e "\033[0;31mError: unable to download cuda_models.json\033[0m"
+  fi
+
 fi
 
-echo -e "\e[96m~/.aisecurity is up to date\e[0m"
+echo -e "\033[0;96m~/.aisecurity is up to date\033[0m"
--- bin/make_keys.sh
+++ bin/make_keys.sh
@@ -21,13 +21,15 @@ function mk_key_files {
   key_loc=$1
 
   if [ ! -f "$key_loc/test_name_keys.txt" ] ; then
+    echo -e "\033[0;95mDownloading test_name_keys.txt\033[0m"
     curl -Lo "test_name_keys.txt" "https://www.dropbox.com/s/yxebmo4gm0qq7nj/test_name_keys.txt?dl=1" || \
-    echo "\e[31mError: could not download test_name_keys.txt\e[0m"
+    echo -e "\033[0;31mError: could not download test_name_keys.txt\033[0m"
   fi
 
   if [ ! -f "$key_loc/test_embedding_keys.txt" ] ; then
+    echo -e "\033[0;95mDownloading test_embedding_keys.txt\033[0m"
     curl -Lo "test_embedding_keys.txt" "https://www.dropbox.com/s/kl5s77evy8m9mpm/test_embedding_keys.txt?dl=1" || \
-    echo "\e[31mError: could not download test_embedding_keys.txt\e[0m"
+    echo -e "\033[0;31mError: could not download test_embedding_keys.txt\033[0m"
   fi
 
 }
@@ -39,11 +41,11 @@ function mk_keys {
   dir=$key_dir
 
   if [ ! -d "$dir" ] ; then
-    echo "\e[95mCreating keys directory at $dir\e[0m"
+    echo -e "\033[0;95mCreating keys directory at $dir\033[0m"
     mkdir "$dir"
   fi
 
-  cd "$dir" || echo "\e[31mError: cannot access $dir\e[0m"
+  cd "$dir" || echo -e "\033[0;31mError: cannot access $dir\033[0m"
 
   mk_key_files "$dir"
 }
@@ -55,7 +57,7 @@ function update_json {
   json_path=$path_to_json
 
   if [ ! -f "$json_path" ] ; then
-    echo "\e[95mCreating json key file at $json_path\e[0m"
+    echo -e "\033[0;95mCreating json key file at $json_path\033[0m"
     touch "$json_path"
     printf '{\n    "names": "%s/test_name_keys.txt",\n    "embeddings": "%s/test_embedding_keys.txt"\n}\n' "$key_dir" "$key_dir" \
     > "$json_path"
@@ -68,3 +70,5 @@ mk_keys "$key_dir"
 
 # update json key file
 update_json "$path_to_json"
+
+echo -e "\033[0;96m~/.aisecurity/keys is up to date\033[0m"
