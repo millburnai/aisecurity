436919c0b9b634f4d7278a7c416a99b48768f5e6
ryan <22parkr@millburn.org>
2020-03-31 10:47:36 -0400

    Remove LCD cls, pbar decorator

--- aisecurity/utils/lcd.py
+++ aisecurity/utils/lcd.py
@@ -5,9 +5,7 @@
 LCD utils.
 
 """
-import concurrent.futures
-import functools
-import time
+
 from timeit import default_timer as timer
 import warnings
 
@@ -20,25 +18,23 @@ from aisecurity.utils import connection
 ################################ Inits ################################
 
 # GLOBALS
-LCD_DEVICE, PROGRESS_BAR = None, None
+PROGRESS_BAR = None
 
 
 # LCD INIT
 def init():
-    global LCD_DEVICE, PROGRESS_BAR
+    global PROGRESS_BAR
 
-    LCD_DEVICE = LCD(mode="sim")
-    LCD_DEVICE.set_message("Loading...\n[ Initializing ]")
-
-    PROGRESS_BAR = LCDProgressBar(lcd=LCD_DEVICE, total=log.THRESHOLDS["num_recognized"])
+    PROGRESS_BAR = LCDProgressBar(mode="sim", total=log.THRESHOLDS["num_recognized"])
+    PROGRESS_BAR.set_message("Loading...\n[ Initializing ]")
 
 
 ################################ Classes ################################
 
-# LCD WRAPPER CLASS (WITH DEV SUPPORT)
-class LCD:
+# LCD PROGRESS BAR
+class LCDProgressBar:
 
-    def __init__(self, mode="pi"):
+    def __init__(self, mode, total, length=16, marker="#"):
         assert mode in ("pi", "sim"), "supported modes are physical (physical LCD) and dev (testing)"
 
         try:
@@ -51,85 +47,46 @@ class LCD:
             if self.mode != mode:
                 warnings.warn("pi lcd mode requested but only simulation lcd available")
 
-
-    # FUNCTIONALITY
-    def set_message(self, message):
-
-        if self.mode == "pi":
-            connection.send({"LCD":message})
-        elif self.mode == "sim":
-            cprint(message, attrs=["bold"])
-
-
-# LCD PROGRESS BAR
-class LCDProgressBar:
-
-    def __init__(self, lcd, total, length=16, marker="#"):
-        self.lcd = lcd
         self.total = total
         self.bar_length = length - 2  # compensate for [] at beginning and end
         self.marker = marker
         self.progress = 0.
         self.empty = " " * self.bar_length
 
+    def set_message(self, message):
+        if self.mode == "pi":
+            connection.send(lcd=message)
+        elif self.mode == "sim":
+            cprint(message, attrs=["bold"])
+
     def reset(self, previous_msg=None):
         self.progress = 0.
+
         if previous_msg:
-            self.lcd.set_message("{}\n[{}]".format(previous_msg, " " * self.bar_length))
+            self.set_message("{}\n[{}]".format(previous_msg, " " * self.bar_length))
 
     def update(self, previous_msg=None):
-        self.progress += 1/self.total
+        self.progress += 1. / self.total
+
+        if self.progress > 1.:
+            self.progress = 1.
 
         done = (self.marker * round(self.progress * self.bar_length) + self.empty)[:self.bar_length]
 
-        self.lcd.set_message("{}\n[{}]".format(previous_msg, done))
+        self.set_message("{}\n[{}]".format(previous_msg, done))
 
-        if bool(self.progress>=1):
+        if self.progress == 1.:
             self.progress = 0.
-
-
-    # PROGRESS BAR DECORATOR
-    @classmethod
-    def progress_bar(cls, lcd, expected_time, msg=None, marker="#"):
-        # https://stackoverflow.com/questions/59013308/python-progress-bar-for-non-loop-function
-
-        def _progress_bar(func):
-
-            def timed_progress_bar(future, expected_time, marker="#", previous_msg=None):
-                # complete early if future completes; wait for future if it doesn't complete in expected_time
-                pbar = cls(total=expected_time, lcd=lcd, marker=marker)
-
-                for sec in range(expected_time - 1):
-                    if future.done():
-                        pbar.update(expected_time - sec, previous_msg=previous_msg)
-                        return
-                    else:
-                        time.sleep(1)
-                        pbar.update(previous_msg=previous_msg)
-
-                future.result()
-                pbar.update(previous_msg=previous_msg)
-
-            @functools.wraps(func)
-            def _func(*args, **kwargs):
-                with concurrent.futures.ThreadPoolExecutor(max_workers=1) as pool:
-                    future = pool.submit(func, *args, **kwargs)
-                    timed_progress_bar(future, expected_time, marker, msg)
-
-                return future.result()
-
-            return _func
-
-        return _progress_bar
 
 
 ################################ Functions ################################
 
 # PERIODIC LCD CLEAR
 def check_clear():
+    global PROGRESS_BAR
+
     lcd_clear = log.THRESHOLDS["num_recognized"] / log.THRESHOLDS["missed_frames"]
     if log.LAST_LOGGED - timer() > lcd_clear or log.UNK_LAST_LOGGED - timer() > lcd_clear:
-        global PROGRESS_BAR
         PROGRESS_BAR.reset()
 
 
@@ -137,4 +94,7 @@ def check_clear():
 def update_progress(update_recognized):
     global PROGRESS_BAR
 
-    PROGRESS_BAR.update(previous_msg="Recognizing...")
+    if update_recognized:
+        PROGRESS_BAR.update(previous_msg="Recognizing...")
+    elif not 1. / PROGRESS_BAR.total + PROGRESS_BAR.progress >= 1.:
+        PROGRESS_BAR.update(previous_msg="Recognizing...")
