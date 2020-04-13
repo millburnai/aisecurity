b8effacb0dcb54866c81d55ab2a8c5b6b02d1533
ryan <22parkr@millburn.org>
2020-04-12 21:05:34 -0400

    Fix haarcascade detection

--- aisecurity/face/detection.py
+++ aisecurity/face/detection.py
@@ -20,7 +20,7 @@ PARAMS = {}
 
 
 # FUNCS
-def detector_init(min_face_size=20, filepath=CONFIG_HOME+"/models/haarcascae_frontalface_default.xml", **kwargs):
+def detector_init(min_face_size=20, filepath=CONFIG_HOME+"/models/haarcascade_frontalface_default.xml", **kwargs):
     global MTCNN, HAARCASCADE, PARAMS
 
     MTCNN = MTCNNBackend(min_face_size=min_face_size, **kwargs)
@@ -36,11 +36,11 @@ def detect_faces(img, mode="mtcnn"):
 
     result = []
 
-    if mode == "mtcnn":
+    if mode == "mtcnn" or mode == "both":
         result = MTCNN.detect_faces(img)
 
     if mode == "haarcascade" or (mode == "both" and not result):
-        min_face_size = PARAMS["min_face_size"]
+        min_face_size = int(round(PARAMS["min_face_size"]))
         faces = HAARCASCADE.detectMultiScale(img, scaleFactor=1.1, minSize=(min_face_size, min_face_size))
 
         for (x, y, width, height) in faces:
