eb2316ac4889f6012526809fc89bf6d05d708c48
ryan <22parkr@millburn.org>
2020-04-30 00:31:13 -0400

    Rename db from LOG to Log

--- bin/drop.sql
+++ bin/drop.sql
@@ -1,9 +1,9 @@
 /* "drop.sql"
-   Drops LOG database and creates an empty one.
+   Drops 'Log' database and creates an empty one.
    */
-DROP DATABASE IF EXISTS LOG;
-CREATE DATABASE LOG;
-USE LOG;
+DROP DATABASE IF EXISTS Log;
+CREATE DATABASE Log;
+USE Log;
 CREATE TABLE Activity (id VARCHAR(5), name VARCHAR(200), date DATE, time TIME);
 /* student_id is VARCHAR(5) for cases like 00001-- can't be represented in python */
 CREATE TABLE Unknown (path_to_img VARCHAR(200), date DATE, time TIME);
