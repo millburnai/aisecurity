e4ae19410c83616db01512599821de4fb2433ffa
ryan <22parkr@millburn.org>
2021-02-18 20:16:25 -0500

    Add compatibility with mac arm64

--- deployment/facenet_test.py
+++ deployment/facenet_test.py
@@ -1,4 +1,9 @@
+import platform
 import sys
+
+if platform.machine() == "arm64":
+    from tensorflow.python.compiler.mlcompute import mlcompute
+    mlcompute.set_mlc_device(device_name="gpu")
 
 sys.path.insert(1, "../")
 from facenet import FaceNet
@@ -6,4 +11,4 @@ from facenet import FaceNet
 
 if __name__ == "__main__":
     facenet = FaceNet()
-    facenet.real_time_recognize(detector="trt-mtcnn", flip=False)
+    facenet.real_time_recognize(detector="mtcnn", flip=False)
