8c40c0a039a3a62376c614f9d60328e7f84786f7
ryan <22parkr@millburn.org>
2021-02-19 23:25:52 -0500

    Update tflite deployment files

--- deployment/to_tflite.py
+++ deployment/to_tflite.py
@@ -1,13 +1,33 @@
-import tensorflow as tf
-converter = tf.compat.v1.lite.TFLiteConverter.from_frozen_graph(
-    "/Users/ethan/aisecurity/config/models/20180402-114759_opt.pb",
-    ["input"],
-    ["embeddings"],
-    {"input":(1, 160, 160, 3)},
-    )
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.target_spec.supported_types = [tf.float16]
-tflite_model = converter.convert()
+import argparse
 
-with open('model.tflite', 'wb') as f:
-    f.write(tflite_model)
+import tensorflow as tf
+
+
+def shape(x):
+    try:
+        return tuple(map(int, x.split(",")))
+    except:
+        raise argparse.ArgumentTypeError("input must be shape tuple")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--infile", help="input filename")
+    parser.add_argument("--outfile", help="output filename")
+    parser.add_argument("--inputs", help="input names", nargs="+")
+    parser.add_argument("--outputs", help="output names", nargs="+")
+    parser.add_argument("--input_shapes", help="input shapes", nargs="+",
+                        type=shape)
+    args = parser.parse_args()
+
+    converter = tf.compat.v1.lite.TFLiteConverter.from_frozen_graph(
+        args.infile, args.inputs, args.outputs,
+        {input_name: shape for input_name, shape
+            in zip(args.inputs, args.input_shapes)},
+    )
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.target_spec.supported_types = [tf.float16]
+    tflite_model = converter.convert()
+
+    with open(args.outfile, "wb") as f:
+        f.write(tflite_model)
