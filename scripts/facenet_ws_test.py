f768a61a91f485dbe2973e1075929be6fe32b420
Ryan Park <ryanpark314@gmail.com>
2022-01-06 20:21:28 -0500

    Module line break pep8

--- scripts/facenet_ws_test.py
+++ scripts/facenet_ws_test.py
@@ -4,6 +4,7 @@ sys.path.insert(1, "../")
 from facenet import FaceNet
 from util.websocket import WebSocket 
 
+
 if __name__ == "__main__":
     facenet = FaceNet()
     ws = WebSocket("172.104.17.15:8000", 1, facenet)
