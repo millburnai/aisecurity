d90e74a13410fd3dedc5806418e7c3d19475142e
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2022-02-10 09:43:05 -0500

    formatted using black

--- util/log.py
+++ util/log.py
@@ -1,11 +1,6 @@
 class Logger:
-
-    def __init__(
-        self,
-        frame_limit: int,
-        frame_threshold
-    ) -> None:
-        # NOTE: Array for matches, sorted chronologically. 
+    def __init__(self, frame_limit: int, frame_threshold) -> None:
+        # NOTE: Array for matches, sorted chronologically.
         # if len > frame_limit, remove index 0
         self.matches = []
         for i in range(frame_limit):
@@ -14,10 +9,7 @@ class Logger:
         self.frame_limit = frame_limit
         self.frame_threshold = frame_threshold
 
-    def log(
-        self,
-        best_match
-    ) -> str:
+    def log(self, best_match) -> str:
         best_match = str(best_match)
         self.matches.append(best_match)
 
@@ -29,18 +21,18 @@ class Logger:
         match_frequencies = {}
 
         # NOTE: Iteration from most recent frame -> last
-        for i in range(self.frame_limit-1, -1, -1): 
+        for i in range(self.frame_limit - 1, -1, -1):
             current_match = self.matches[i]
             # NOTE: If match id doesn't exist, set frequency to 1
             if current_match not in match_frequencies.keys():
-                match_frequencies.update({current_match : 1})
+                match_frequencies.update({current_match: 1})
 
             # NOTE: Increment frequency
             else:
                 match_frequencies.update(
-                        {current_match: match_frequencies.get(current_match) + 1}
+                    {current_match: match_frequencies.get(current_match) + 1}
                 )
-                #NOTE: if frequency == threshold, detect
+                # NOTE: if frequency == threshold, detect
                 if match_frequencies.get(current_match) >= self.frame_threshold:
                     return current_match
 
